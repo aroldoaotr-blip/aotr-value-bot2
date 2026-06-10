eca0ab92fc90bd05b39acdb83c5da2d2fc15b64f
Aroldo Jerez <aroldojerez01@gmail.com>
2026-06-09 22:35:07 -0300

    add

--- src/data/searchAliases.js
+++ src/data/searchAliases.js
@@ -96,5 +96,6 @@ export const SEARCH_ALIASES = {
     "collar de cristal":"crystal necklace",
     "cristal":"crystal",
     "collar":"necklace",
-"collar femenino":"crystal necklace",
+    "collar femenino":"crystal necklace",
+    "demonio":"devil"
 };
